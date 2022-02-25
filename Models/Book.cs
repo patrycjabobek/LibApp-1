ba64c505eeac58af37564d31b19e0d962b2809f3
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2022-02-25 17:06:16 +0100

    Added validation on client and server side for BookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,18 +8,24 @@ namespace LibApp.Models
 {
     public class Book
     {
-        public int Id { get; set; }
-		[Required]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Name is required")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "AuthorName is required")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required")]
 		public Genre Genre { get; set; }
+		[Required(ErrorMessage = "GenreId is required")]
 		public byte GenreId { get; set; }
+		[Required(ErrorMessage = "DateAdded is required")]
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "ReleaseDate is required")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "NumberInStock is required")]
+		[Range(1, 20, ErrorMessage = "NumberInStock must be between 1 and 20")]
 		public int NumberInStock { get; set; }
+		[Required(ErrorMessage = "NumberAvailable is required")]
 		public int NumberAvailable { get; set; }
 	}
       
